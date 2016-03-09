f6008ef5e4ffc7a1dcced53579f52e50ee226d55
leBoer <christianborre@gmail.com>
2016-03-09 21:10:22 +0800

    Removes all error messages, and the character now moves around.

--- js/app.js
+++ js/app.js
@@ -24,11 +24,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(loc){
-    var obj = Object.create(Player.prototype);
-    obj.loc = loc;
-    this.sprite = 'char-boy.png';
-    return obj;
+var Player = function(x, y){
+//    var obj = Object.create(Player.prototype);
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+
 };
 
 Player.prototype.update = function(dt){
@@ -37,11 +38,25 @@ Player.prototype.update = function(dt){
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Player.prototype.handleInput = function(e){
+    var e = e;
+    if(e == 'left'){
+        player.x = player.x - 101;
+    }else if (e == 'right'){
+        player.x = player.x + 101;
+    }else if (e == 'up'){
+        this.y = this.y - 101;
+    }else if (e == 'down'){
+        this.y = this.y + 101;
+    }
+};
+
 // Now instantiate your objects
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var player = Player(3);
+var player = new Player(1,2);
 console.log(player);
 
 // This listens for key presses and sends the keys to your
@@ -53,6 +68,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
